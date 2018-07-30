8f3770a8ac6d723d4c97361a8b5887bc288dbfea
JokerAlex <alex.zhao1023@gmail.com>
2018-07-30 08:55:33 +0800

    登录测试

--- src/test/java/com/dzkd/website/service/LoginServiceTest.java
+++ src/test/java/com/dzkd/website/service/LoginServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LoginServiceTest {
@@ -17,8 +19,23 @@ public class LoginServiceTest {
     private LoginService loginService;
 
     @Test
-    public void test(){
-        JSONObject jsonObject = loginService.login("test", "111");
-        System.out.println(jsonObject.toString());
+    public void test() {
+        JSONObject adminAndStudent = loginService.login("test", "111");
+        System.out.println("Admin And Student\n" + adminAndStudent.toString());
+
+        JSONObject admin = loginService.login("test2", "222");
+        System.out.println("Admin\n" + admin.toString());
+
+        JSONObject student = loginService.login("test3", "333");
+        System.out.println("Student\n" + student.toString());
+
+        JSONObject wrongPass = loginService.login("test", "222");
+        System.out.println("wrongPass\n" + wrongPass.toString());
+    }
+
+    @Test
+    public void time() {
+        System.out.println("Date:" + new Date().toString());
+        System.out.println("System:" + System.currentTimeMillis());
     }
 }
