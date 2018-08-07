4d17486df695e2505049e2930c2a87e69e191f60
JokerAlex <alex.zhao1023@gmail.com>
2018-08-07 21:01:56 +0800

    文件操作

--- src/main/java/com/dzkd/website/controller/FileController.java
+++ src/main/java/com/dzkd/website/controller/FileController.java
@@ -12,16 +12,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FileController {
 
     private static final Logger logger = LogManager.getLogger(FileController.class);
 
-    private static String FILE_PATH = "/Users/alex/Downloads/uploadFiles/files/";
 
     private PictureMapper pictureMapper;
     private FileInfoMapper fileInfoMapper;
@@ -33,9 +37,11 @@ public class FileController {
     }
 
 
-
     /**
      * 文件上传
+     * 招生信息文件type:0,articleCate:0
+     * 新闻图片type:1,articleCate:0
+     *
      * @param file
      * @param type
      * @param articleId
@@ -44,22 +50,14 @@ public class FileController {
      */
     @RequestMapping(value = "/admin/upload", method = RequestMethod.POST)
     public R upload(@RequestParam("file") MultipartFile file, int type, int articleId, int articleCate) {
+
         String fileName = file.getOriginalFilename();
-        String filePath;
 
-        if (type == 0) {
-            filePath = FILE_PATH + articleCate + "/" + articleId + "/";
-        } else if (type ==1) {
-            filePath = "/Users/alex/Downloads/uploadFiles/pictures/" + articleCate + "/" + articleId + "/";
-        } else {
-            return R.isFail(new Exception("参数错误"));
-        }
-
-        logger.info("filePath------>" + filePath + fileName);
+        logger.info("fileName------>" + fileName);
         logger.info("type-->" + type + "\tarticleId-->" + articleId + "\tarticleCate-->" + articleCate);
 
         try {
-            FileUtil.uploadFile(file, filePath, fileName);
+            FileUtil.uploadFile(file, fileName, type, articleCate, articleId);
             if (type == 0) {
                 FileInfo fileInfo = new FileInfo();
                 fileInfo.setFileArticleCate(articleCate);
@@ -75,15 +73,57 @@ public class FileController {
                 int insertPicture = pictureMapper.insertSelective(picture);
                 logger.info("FileController->insertPicture:" + insertPicture);
             }
-
             return R.isOk();
         } catch (Exception e) {
             return R.isFail(e);
         }
     }
 
+    @RequestMapping(value = "/admin/delFile", method = RequestMethod.DELETE)
+    public R delFiles(@RequestBody Map<String, Integer> map) {
+        int id = map.get("id");
+        int type = map.get("type");
+        logger.info("id-->" + id + "type-->" + type);
+
+        try {
+            List<Object> fileList = new ArrayList<>();
+            if (type == 0) {
+                FileInfo fileInfo = fileInfoMapper.selectByPrimaryKey(id);
+                if (fileInfo == null) {
+                    return R.isFail(new Exception("Not Found"));
+                }
+                fileList.add(fileInfo);
+                FileUtil.delFile(fileList,type);
+                int delFile = fileInfoMapper.deleteByPrimaryKey(id);
+                logger.info("delFiles->delFile" + delFile);
+            } else if (type == 1) {
+                Picture picture = pictureMapper.selectByPrimaryKey(id);
+                if (picture == null) {
+                    return R.isFail(new Exception("Not Found"));
+                }
+                fileList.add(picture);
+                FileUtil.delFile(fileList,type);
+                int delPicture = pictureMapper.deleteByPrimaryKey(id);
+                logger.info("delFiles->delPicture" + delPicture);
+            } else {
+                return R.isFail(new Exception("参数错误"));
+            }
+
+            return R.isOk();
+        } catch (Exception e) {
+            logger.catching(e);
+            return R.isFail(e);
+        }
+    }
+
+    @RequestMapping(value = "/admin/showFiles", method = RequestMethod.GET)
+    public R showFiles(int articleCate, int articleId) {
+        List<FileInfo> fileInfoList = fileInfoMapper.selectByArticle(articleCate, articleId);
+        return R.isOk().data(fileInfoList);
+    }
+
     @RequestMapping("/download/{fileId}")
-    public R download(HttpServletResponse response, @PathVariable(name = "fileId") Integer fileId) {
+    public R download(HttpServletRequest request, HttpServletResponse response, @PathVariable(name = "fileId") Integer fileId) {
         if (fileId == null) {
             return R.isFail(new Exception("参数错误"));
         }
@@ -94,25 +134,26 @@ public class FileController {
             return R.isFail(new Exception("请求的资源不存在"));
         }
 
-        String filePath = FILE_PATH + fileInfo.getFileArticleCate() + "/" + fileInfo.getFileArticleId() + "/";
-        logger.info("download->fileInfo:" + filePath + fileInfo.getFileName());
-
-        File file = new File(filePath, fileInfo.getFileName());
+        String fileName;
+        try {
+            if (request.getHeader("User-Agent").toUpperCase().indexOf("MSIE") > 0) {
+                fileName = URLEncoder.encode(fileInfo.getFileName(), "UTF-8");
+            } else {
+                fileName = new String(fileInfo.getFileName().getBytes(StandardCharsets.UTF_8), "ISO8859-1");
+            }
+        } catch (UnsupportedEncodingException e) {
+            logger.catching(e);
+            return R.isFail(new Exception("编码错误"));
+        }
 
         //response.setContentType("multipart/form-data");
         //也可以明确的设置一下UTF-8，测试中不设置也可以。
         response.setContentType("multipart/form-data;charset=UTF-8");
-        response.setHeader("Content-Disposition", "attachment; fileName="+
-                fileInfo.getFileName());
-
-
-        /*//response.setContentType("application/force-download");// 设置强制下载不打开
-        response.setHeader("content-type", "application/octet-stream");
-        response.setContentType("application/octet-stream");
-        response.addHeader("Content-Disposition", "attachment;fileName=" + fileInfo.getFileName());// 设置文件名*/
+        response.setHeader("Content-Disposition", "attachment; fileName=" +
+                fileName);
 
         try {
-            FileUtil.downloadFile(file, response.getOutputStream());
+            FileUtil.downloadFile(fileInfo, response.getOutputStream());
             return R.isOk();
         } catch (Exception e) {
             logger.catching(e);
--- src/main/java/com/dzkd/website/util/FileUtil.java
+++ src/main/java/com/dzkd/website/util/FileUtil.java
@@ -1,27 +1,62 @@
 package com.dzkd.website.util;
 
-import com.dzkd.website.pojo.R;
+import com.dzkd.website.pojo.FileInfo;
+import com.dzkd.website.pojo.Picture;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.util.List;
+import java.util.Objects;
 
 public class FileUtil {
 
+    private static String FILE_PATH = "/Users/alex/Downloads/uploadFiles/files/";
+    private static String PICTURE_PATH = "/Users/alex/Downloads/uploadFiles/pictures/";
+    private static int FILE = 0;
+    private static int PICTURE = 1;
+
     private static final Logger logger = LogManager.getLogger(FileUtil.class);
 
-    public static void uploadFile(MultipartFile file, String filePath, String fileName) throws Exception {
+
+    /**
+     * 招生信息文件type:0,articleCate:0
+     * 新闻图片type:1,articleCate:0
+     *
+     * @param file
+     * @param fileName
+     * @param fileType
+     * @throws Exception
+     */
+    public static void uploadFile(MultipartFile file, String fileName, int fileType, int articleCate, int articleId) throws Exception {
         if (file.isEmpty()) {
             throw new Exception("文件为空");
         }
+
         try {
             byte[] fileByte = file.getBytes();
-            File targetFile = new File(filePath);
-            if(!targetFile.exists()){
-                targetFile.mkdirs();
+
+            File targetFile;
+            FileOutputStream out;
+
+            if (fileType == FILE) {
+                targetFile = new File(FILE_PATH + articleCate + "/" + articleId + "/");
+                if (!targetFile.exists()) {
+                    targetFile.mkdirs();
+                }
+                out = new FileOutputStream(FILE_PATH + articleCate + "/" + articleId + "/" + fileName);
+            } else if (fileType == PICTURE) {
+                targetFile = new File(PICTURE_PATH + articleCate + "/" + articleId + "/");
+                if (!targetFile.exists()) {
+                    targetFile.mkdirs();
+                }
+                out = new FileOutputStream(PICTURE_PATH + articleCate + "/" + articleId + "/" + fileName);
+            } else {
+                throw new Exception("文件类型不正确");
             }
-            FileOutputStream out = new FileOutputStream(filePath+fileName);
+
+
             out.write(fileByte);
             out.flush();
             out.close();
@@ -32,7 +67,18 @@ public class FileUtil {
         }
     }
 
-    public static void downloadFile(File file, OutputStream outputStream) throws Exception {
+    /**
+     * 下载文件
+     * @param fileInfo
+     * @param outputStream
+     * @throws Exception
+     */
+    public static void downloadFile(FileInfo fileInfo, OutputStream outputStream) throws Exception {
+        File file = new File(FILE_PATH
+                + fileInfo.getFileArticleCate() + "/"
+                + fileInfo.getFileArticleId() + "/", fileInfo.getFileName());
+        logger.info("fileInfo-->" + file.getPath() + file.getName());
+
         if (!file.exists()) {
             throw new Exception("请求的资源不存在");
         }
@@ -68,5 +114,45 @@ public class FileUtil {
                 }
             }
         }
+    }
+
+    /**
+     * 删除文件
+     * @param fileInfoList
+     * @param type
+     * @throws Exception
+     */
+    public static void delFile(List fileInfoList, int type) throws Exception {
+        for (Object object : fileInfoList) {
+            File file;
+            if (type == FILE) {
+                FileInfo fileInfo = (FileInfo) object;
+                file = new File(FILE_PATH
+                        + fileInfo.getFileArticleCate() + "/"
+                        + fileInfo.getFileArticleId() + "/", fileInfo.getFileName());
+            } else if (type == PICTURE) {
+                Picture picture = (Picture) object;
+                file = new File(PICTURE_PATH
+                        + picture.getPictureCategory() + "/"
+                        + picture.getAcessId() + "/", picture.getPictureName());
+            } else {
+                throw new Exception("文件类型错误");
+            }
+
+            // 路径为文件且不为空则进行删除
+            if (file.isFile() && file.exists()) {
+                System.gc();    //加上确保文件能删除，不然可能删不掉
+                boolean flag = file.delete();
+                logger.info("flag-->" + file.getName() + "-->" + flag);
+            }
+
+            File fileDir = new File(file.getParent());
+            if (fileDir.isDirectory() && Objects.requireNonNull(fileDir.list()).length == 0) {
+                System.gc();    //加上确保文件能删除，不然可能删不掉
+                boolean flag = fileDir.delete();
+                logger.info("flag-->" + fileDir.getPath() + "-->" + flag);
+            }
+        }
+
     }
 }
